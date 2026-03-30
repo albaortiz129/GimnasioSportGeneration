d1b586099498a02df1ff4b4e1d48f9dedbe50356
Alba <alba.ortiz129@gmail.com>
2026-03-30 20:58:04 +0200

    Update start.sh

--- seafit-app/start.sh
+++ seafit-app/start.sh
@@ -9,10 +9,16 @@ mkdir -p /var/www/html/storage/framework/views \
     /var/www/html/storage/logs \
     /var/www/html/bootstrap/cache
 
+touch /var/www/html/storage/logs/laravel.log
+
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+
 php artisan config:clear || true
 php artisan cache:clear || true
 php artisan view:clear || true
-
 php artisan migrate --force || true
+
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 apache2-foreground
